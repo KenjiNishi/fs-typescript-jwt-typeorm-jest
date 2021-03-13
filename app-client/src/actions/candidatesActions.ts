7bac1bbd7d35f6f2e069a9e688543afeb3b20d74
KenjiNishi <wk.nishizawa@gmail.com>
2021-03-13 19:25:08 -0300

    remaining crud actions and reducers for candidates

--- app-client/src/actions/candidatesActions.ts
+++ app-client/src/actions/candidatesActions.ts
@@ -2,7 +2,7 @@ import { ActionCreator, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import axios, { AxiosResponse } from 'axios';
 
-import { LIST_CANDIDATES, CREATE_CANDIDATE, DELETE_CANDIDATE, UPDATE_CANDIDATE} from './types';
+import { LIST_CANDIDATES, CREATE_CANDIDATE, GET_CANDIDATE, UPDATE_CANDIDATE, DELETE_CANDIDATE } from './types';
 import {candidatesStateInterface} from '../reducers/candidatesReducers';
 import { returnErrors, createMessage } from './alertsActions';
 
@@ -13,12 +13,20 @@ export interface CandidateInterface{
     age: number;
     linkedin: string;
     techs: string[];
-    createdAt:Date;
+    //createdAt:Date;
+}
+
+export interface candidatesActionListI {
+    type : typeof LIST_CANDIDATES
+    payload : CandidateInterface[]
 }
 
 export interface candidatesActionGetI {
-    type : typeof LIST_CANDIDATES
-    payload : CandidateInterface[]
+    type : typeof GET_CANDIDATE | typeof UPDATE_CANDIDATE
+    payload : CandidateInterface
+}
+export interface candidatesActionsNoPayloadI {
+    type: typeof DELETE_CANDIDATE | typeof CREATE_CANDIDATE
 }
 
 
@@ -27,7 +35,7 @@ export const listCandidates : ActionCreator<
         Promise<any>, 
         candidatesStateInterface, 
         null, 
-        candidatesActionGetI
+        candidatesActionListI
     >>  = ( token : string) =>{
 
         return async (dispatch : Dispatch) =>{
@@ -44,6 +52,124 @@ export const listCandidates : ActionCreator<
                         type: LIST_CANDIDATES,
                         payload: response.data
                     });
+                }).catch((err) => {
+                    if(!err.response){dispatch(returnErrors(err.message, 500));}
+                    else{dispatch(returnErrors(err.message, err.response.status));}
+                })
+    }
+}
+
+export const createCandidate : ActionCreator<
+    ThunkAction<
+        Promise<any>, 
+        candidatesStateInterface, 
+        null, 
+        candidatesActionsNoPayloadI
+    >>  = ( token : string, candidate : CandidateInterface) =>{
+
+        return async (dispatch : Dispatch) =>{
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+            const body = JSON.stringify(candidate);
+            
+            await axios.post(`http://localhost:8080/api/candidates/create/`, body, config)
+                .then((response : AxiosResponse) => {
+                    dispatch(createMessage(`Candidate created!`));
+                    dispatch({
+                        type: CREATE_CANDIDATE
+                    })
+                }).catch((err) => {
+                    if(!err.response){dispatch(returnErrors(err.message, 500));}
+                    else{dispatch(returnErrors(err.message, err.response.status));}
+                })
+    }
+}
+
+export const getCandidate : ActionCreator<
+    ThunkAction<
+        Promise<any>, 
+        candidatesStateInterface, 
+        null, 
+        candidatesActionGetI
+    >>  = ( token : string, id : string) =>{
+
+        return async (dispatch : Dispatch) =>{
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+            await axios.get(`http://localhost:8080/api/candidates/get/${id}`, config)
+                .then((response : AxiosResponse) => {
+                    dispatch(createMessage(`Getting Candidate information...`));
+                    dispatch({
+                        type: GET_CANDIDATE,
+                        payload: response.data
+                    });
+                }).catch((err) => {
+                    if(!err.response){dispatch(returnErrors(err.message, 500));}
+                    else{dispatch(returnErrors(err.message, err.response.status));}
+                })
+    }
+}
+
+export const updateCandidate : ActionCreator<
+    ThunkAction<
+        Promise<any>, 
+        candidatesStateInterface, 
+        null, 
+        candidatesActionGetI
+    >>  = ( id: string , token : string, candidate : CandidateInterface) =>{
+
+        return async (dispatch : Dispatch) =>{
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+            const body = JSON.stringify(candidate);
+            
+            await axios.put(`http://localhost:8080/api/candidates/update/${id}`, body, config)
+                .then((response : AxiosResponse) => {
+                    dispatch(createMessage(`Candidate updated!`));
+                    dispatch({
+                        type: UPDATE_CANDIDATE,
+                        payload: response.data
+                    })
+                }).catch((err) => {
+                    if(!err.response){dispatch(returnErrors(err.message, 500));}
+                    else{dispatch(returnErrors(err.message, err.response.status));}
+                })
+    }
+}
+
+export const deleteCandidate : ActionCreator<
+    ThunkAction<
+        Promise<any>, 
+        candidatesStateInterface, 
+        null, 
+        candidatesActionsNoPayloadI
+    >>  = ( id: string , token : string) =>{
+
+        return async (dispatch : Dispatch) =>{
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+            await axios.delete(`http://localhost:8080/api/candidates/delete/${id}`, config)
+                .then((response : AxiosResponse) => {
+                    dispatch(createMessage(`Candidate deleted: ${id}`));
+                    dispatch({
+                        type: DELETE_CANDIDATE
+                    })
                 }).catch((err) => {
                     if(!err.response){dispatch(returnErrors(err.message, 500));}
                     else{dispatch(returnErrors(err.message, err.response.status));}
--- app-client/src/reducers/candidatesReducers.ts
+++ app-client/src/reducers/candidatesReducers.ts
@@ -1,21 +1,31 @@
-import { LIST_CANDIDATES} from '../actions/types';
-import { CandidateInterface, candidatesActionGetI } from '../actions/candidatesActions';
+import { CREATE_CANDIDATE, DELETE_CANDIDATE, GET_CANDIDATE, LIST_CANDIDATES, UPDATE_CANDIDATE} from '../actions/types';
+import { CandidateInterface, candidatesActionListI, candidatesActionsNoPayloadI, candidatesActionGetI } from '../actions/candidatesActions';
+import { useDispatch } from 'react-redux';
 
 export interface candidatesStateInterface{
     candidates : CandidateInterface[];
+    selectedCandidate : CandidateInterface;
 }
 
+const nullCandidate = {id:'',name:'', email:'', age:0, linkedin:'', techs:[]}
 const initialState : candidatesStateInterface = {
-    candidates: []
+    candidates: [],
+    selectedCandidate: nullCandidate
 };
 
 export const candidatesReducer  = (
     state = initialState,
-    action : candidatesActionGetI
+    action : candidatesActionListI | candidatesActionsNoPayloadI | candidatesActionGetI
     ) => {
         switch (action.type){
             case LIST_CANDIDATES:
                 return{...state, candidates : action.payload}
+            case GET_CANDIDATE:
+            case UPDATE_CANDIDATE:
+                return{...state, selectedCandidate : action.payload}
+            case CREATE_CANDIDATE:
+            case DELETE_CANDIDATE:
+                return {...state, selectedCandidate : nullCandidate}
             default:
                 return state;
         }
